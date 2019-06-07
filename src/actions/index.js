039d6afecad89a714092feb68b1edd04aab53405
Prateek <prateeksoni47@iaugmentor.com>
2019-06-07 10:41:44 +0530

    Refactor authActions

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,10 +1,10 @@
-import authApi from "../apis/login";
 import pyLearningApi from "../apis/pylearning";
 
 import qs from "querystring";
 import * as ACTION_TYPE from "./actionTypes";
 import history from "../history";
 import { getOrganizationModules } from "./organization";
+import { loginUser, logoutUser } from "./authActions";
 import {
   fetchQuestionList,
   fetchQuestionDetail,
@@ -13,23 +13,11 @@ import {
 
 export { fetchQuestionList, fetchQuestionDetail, updateQuestion };
 export { getOrganizationModules };
+export { loginUser, logoutUser };
 
 // >>>>>>>>>>>>>>>>>>>>>>>>>REFACTOR BELOW>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 
 // USER
-export const loginUser = formValues => async dispatch => {
-  const response = await authApi.post(
-    "/v2/accounts/auth/signin/",
-    qs.stringify(formValues)
-  );
-  dispatch({ type: ACTION_TYPE.LOGIN_USER, payload: response.data });
-};
-
-export const logoutUser = () => {
-  return {
-    type: ACTION_TYPE.LOGOUT_USER
-  };
-};
 
 export const setUserAuthValue = formValues => {
   return {
