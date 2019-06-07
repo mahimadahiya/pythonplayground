46d544d6a212f0b5f1ba4126c943e598138ae1cc
Prateek <prateeksoni47@iaugmentor.com>
2019-06-07 09:22:08 +0530

    Saved to branch prateek

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,15 +1,8 @@
 import { combineReducers } from "redux";
-<<<<<<< HEAD
-import { reducer as formReducer } from "redux-form";
-import authReducer from './authReducer'
-import moduleTrackReducer from './moduleTrackReducer'
-import organizationReducer from './organizationReducer'
-=======
 import authReducer from "./authReducer";
 import moduleTrackReducer from "./moduleTrackReducer";
 import organizationReducer from "./organizationReducer";
 import questionReducer from "./questionReducer";
->>>>>>> 1ac65ca... Added error boundary
 
 export default combineReducers({
   userAuth: authReducer,
