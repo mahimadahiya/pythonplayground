652bcbeeb3ed95d9e658687cdea4fefcb5f0d20f
Prateek <prateeksoni47@iaugmentor.com>
2019-06-10 16:59:03 +0530

    Removed formik from Create Track

--- src/components/Screens/ModuleTracks/create.jsx
+++ src/components/Screens/ModuleTracks/create.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 import { connect } from "react-redux";
 import DisplayFormTrackCreate from "../Form/DisplayFormTrackCreate";
-import { Card } from "antd";
+import { Card, Form, Input, Select, DatePicker } from "antd";
 import { Formik } from "formik";
 import { dateFormat } from "../Form/FieldFormats";
 
 import { createModuleTrack, fetchOrganizations } from "../../../actions";
 import moment from "moment";
+import MButton from "../../Elements/MButton";
 
 class CreateTrack extends React.Component {
   componentDidMount() {
@@ -14,22 +15,94 @@ class CreateTrack extends React.Component {
     this.props.heading("Create Track");
   }
 
-  onSubmit = formProps => {
-    const { liveDate, orgId, trackName } = formProps;
-    const selectedDate = moment(liveDate).format(dateFormat);
-    const formValues = {
-      name: trackName,
-      organization_id: orgId,
-      going_live_at: selectedDate
-    };
-    this.props.createModuleTrack(this.props.user.Authorization, formValues);
+  onSubmit = e => {
+    e.preventDefault();
+    this.props.form.validateFields((err, formProps) => {
+      if (!err) {
+        const selectedDate = moment(formProps.going_live_at).format(dateFormat);
+        const formValues = {
+          ...formProps,
+          going_live_at: selectedDate
+        };
+        this.props.createModuleTrack(this.props.user.Authorization, formValues);
+      } else {
+        console.log(err);
+      }
+    });
+  };
+
+  filterOrganizations = (val, option) => {
+    const filteredList = this.props.organizations.filter(({ name }) => {
+      if (name.toLowerCase().includes(val) || option.key.includes(val)) {
+        return true;
+      }
+      return false;
+    });
+    for (var i = 0; i < filteredList.length; i++) {
+      if (filteredList[i].id.toString() === option.key) return true;
+    }
+    return false;
   };
 
   render() {
+    const { getFieldDecorator } = this.props.form;
     return (
       <div>
         <Card>
-          <Formik
+          <Form onSubmit={this.onSubmit}>
+            <Form.Item label="Track Name">
+              {getFieldDecorator("name", {
+                rules: [
+                  {
+                    required: true,
+                    message: "Please enter a valid Track Name"
+                  }
+                ]
+              })(<Input placeholder="Type Track Name" />)}
+            </Form.Item>
+            <Form.Item label="Organization">
+              {getFieldDecorator("organization_id", {
+                rules: [
+                  {
+                    required: true,
+                    message: "Please select an Organization"
+                  }
+                ]
+              })(
+                <Select
+                  placeholder="Select an Organization"
+                  allowClear
+                  showSearch
+                  filterOption={this.filterOrganizations}
+                >
+                  {this.props.organizations.map(organization => {
+                    return (
+                      <Select.Option
+                        value={organization.id}
+                        key={organization.id}
+                      >{`${organization.name} (${
+                        organization.id
+                      })`}</Select.Option>
+                    );
+                  })}
+                </Select>
+              )}
+            </Form.Item>
+            <Form.Item label="Going Live At">
+              {getFieldDecorator("going_live_at", {
+                rules: [
+                  {
+                    required: true,
+                    message: "Please select a date"
+                  }
+                ]
+              })(<DatePicker />)}
+            </Form.Item>
+            <Form.Item>
+              <MButton>Create Track</MButton>
+            </Form.Item>
+          </Form>
+          {/* <Formik
             onSubmit={this.onSubmit}
             render={formikProps => (
               <DisplayFormTrackCreate
@@ -37,7 +110,7 @@ class CreateTrack extends React.Component {
                 list={this.props.organizations}
               />
             )}
-          />
+          /> */}
         </Card>
       </div>
     );
@@ -54,4 +127,4 @@ const mapStateToProps = state => {
 export default connect(
   mapStateToProps,
   { createModuleTrack, fetchOrganizations }
-)(CreateTrack);
+)(Form.create()(CreateTrack));
