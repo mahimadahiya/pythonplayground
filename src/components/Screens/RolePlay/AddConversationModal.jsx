ffde8a17ca0389cb651ef685324f1135ea647dde
videsh <videsh.ghodarop@iaugmentor.com>
2019-10-16 18:25:37 +0530

    working on adding conversation

--- src/components/Screens/RolePlay/AddConversationModal.jsx
+++ src/components/Screens/RolePlay/AddConversationModal.jsx
@@ -5,13 +5,46 @@ import {
   Input,
   Modal,
   Button,
-  Select
+  Select,
+  message
 } from "antd";
 
 import "./index.css";
 const { Option } = Select;
 
  const AddConversationModal = props => {
+
+    const [type,SetType] = useState();
+    const [text,SetText] = useState();
+    const [timer,SetTimer] = useState();
+
+    const onSelectTypeChange = (value) =>{
+        SetType(value);
+    }
+
+    const onTextChange = (event) =>{
+        SetText(event.target.value);
+    }
+
+    const onTimerChange = (event) =>{
+        SetTimer(event.target.value);
+    }
+
+    const onAddingConversation = async () => {
+
+        if(type === null ||
+            type === "" ||
+            type === " " ||
+            type === undefined
+            
+            ){
+                message.warning("Type is Null");
+                return;
+            }
+    }
+
+
+
 
 
     return (
@@ -24,14 +57,14 @@ const { Option } = Select;
                 destroyOnClose={true}
                 footer={false}
             >
-             
+             <div>
              <div>
              <Select
                  showSearch
                  style={{ width: 200 }}
                  placeholder="Select Type"
                  optionFilterProp="children"
-                
+                 onChange={onSelectTypeChange}
              >
                 <Option value="Full Overlay">Full Overlay</Option>
                 <Option value="Right Overlay">Right Overlay</Option>
@@ -41,6 +74,27 @@ const { Option } = Select;
             </Select>
              </div>
 
+             <div style={{marginTop:"30px"}}>
+             <Input
+              style={{ maxWidth: "450px", width: "100%" }}
+              onChange={onTextChange}
+              placeholder="Text"
+             />
+             </div>
+
+             <div style={{marginTop:"30px"}}>
+             <Input
+              style={{ maxWidth: "450px", width: "100%" }}
+              type="number"
+              placeholder="Timer"
+              onChange={onTimerChange}
+             />
+             </div>
+
+             <div style={{marginTop:"30px",textAlign:"center"}}>
+             <Button type="primary" onClick={onAddingConversation} >Add </Button>
+             </div>
+             </div>
             </Modal>
 
         </div>
