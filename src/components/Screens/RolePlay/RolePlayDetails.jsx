f1172812d77c293be124b7a932a994674eb91f53
videsh <videsh.ghodarop@iaugmentor.com>
2019-10-16 15:50:49 +0530

    conversation list added

--- src/components/Screens/RolePlay/RolePlayDetails.jsx
+++ src/components/Screens/RolePlay/RolePlayDetails.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
-import { Row,Col } from "antd";
+import { Row,Col,List } from "antd";
 import { rolePlayConversationDetails } from "../../../actions";
 
 const RolePlayDetails = props => {
@@ -13,6 +13,7 @@ const RolePlayDetails = props => {
     const [avatarTwoImage,setAvatarTwoImage] = useState();
 
     const [backgroundImage,setBackgroundImage] = useState();
+    const [conversationDetails,setConversationDetails] = useState();
     
     useEffect(() => {
         const fetchDetails = async () => {
@@ -21,12 +22,14 @@ const RolePlayDetails = props => {
             user.Authorization,
             props.match.params.id
           );
-          console.log(details.result.rp_article_details);
+          console.log(details.result.article_conversation);
           setAvatarOneName(details.result.rp_article_details.avatar_details[0].name);
           setAvatarTwoName(details.result.rp_article_details.avatar_details[1].name);
           setAvatarOneImage(details.result.rp_article_details.avatar_details[0].media_url);
           setAvatarTwoImage(details.result.rp_article_details.avatar_details[1].media_url);
           setBackgroundImage(details.result.rp_article_details.background_url);
+          setConversationDetails(details.result.article_conversation);
+          
           
         };
         fetchDetails();
@@ -35,30 +38,54 @@ const RolePlayDetails = props => {
     return (
         <div>
             <div style={{textAlign:"center",background:"lightblue"}}><h3 style={{lineHeight:"44px"}}>Role Play</h3></div>
-              <div style={{border:"1px solid black",margin:"20px",borderRadius:"36px"}}>
+              <div style={{border:"1px solid #999999",margin:"20px",borderRadius:"5px",padding:"35px"}}>
                   <Row style={{marginTop:"50px"}}>
                       <Col sm={22} md={16} lg={16} style={{marginLeft:"40px"}}>
                           <div style={{display:"flex",marginBottom:"30px"}}>
-                              <div style={{width:"40%"}}> <h3 style={{border:"1px solid black",padding:"12px",marginBottom:"30px"}} >{avatarOneName}</h3></div>
-                              <div style={{width:"50%",textAlign:"center"}}> <img src={avatarOneImage} style={{width:"90px"}} /> </div>
+                              <div style={{width:"40%"}}> <h3 style={{border:"1px solid #999999",padding:"12px",marginBottom:"30px"}} >{avatarOneName}</h3></div>
+                              <div style={{width:"50%",textAlign:"center"}}> <img src={avatarOneImage} style={{width:"90px"}} alt="leftAvatar" /> </div>
                           </div>
 
                           <div style={{display:"flex"}}>
-                              <div style={{width:"40%"}}> <h3 style={{border:"1px solid black",padding:"12px",marginBottom:"30px"}} >{avatarTwoName}</h3></div>
-                              <div style={{width:"50%",textAlign:"center"}}> <img src={avatarTwoImage} style={{width:"90px"}} /> </div>
+                              <div style={{width:"40%"}}> <h3 style={{border:"1px solid #999999",padding:"12px",marginBottom:"30px"}} >{avatarTwoName}</h3></div>
+                              <div style={{width:"50%",textAlign:"center"}}> <img src={avatarTwoImage} style={{width:"90px"}} alt="rightAvatar" /> </div>
                           </div>
                          
                           
                       </Col>
                       <Col sm={22} md={7} lg={7}>
                             <div style={{textAlign:"center"}}>
-                                <img style={{width:"120px",height:"150px"}} src={backgroundImage} />
+                                <div>
+                                <img style={{width:"120px",height:"150px"}} src={backgroundImage} alt="background" />
+                                </div>
+                                <div style={{marginTop:"10px"}}>
+                                    <span style={{fontWeight:"bold"}}>Background</span>
+                                </div>
                             </div>
                       </Col>
                       
                   </Row>
               </div>
+
+              <div style={{textAlign:"center",background:"lightblue"}}><h3 style={{lineHeight:"44px"}}>Sequence</h3></div>
+              <div style={{padding:"35px"}}>
+              <List
+                  itemLayout="horizontal"
+                  dataSource={conversationDetails}
+                  renderItem={item => (
+                <List.Item>
+                     <List.Item.Meta style={{border:"1px solid #999999",padding:"20px",borderRadius:"5px"}}
+          
+                         title={<div><span>{item.type}</span><span style={{float:"right"}}>{item.timer}</span></div>}
+                         description={item.text}
+                 />
+                </List.Item>
+                 )}
+               />
+              </div>
+
         </div>
+        
     );
 };
 
