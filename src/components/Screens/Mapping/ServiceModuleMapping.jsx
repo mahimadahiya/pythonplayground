2fb6d404704da6e619d151805c5cdeff80a38084
gauraV <gauravsaini964@gmail.com>
2019-11-07 13:42:51 +0530

    Fix module_name being send instead of module_id when adding modules to existing Service.
    Signed-off-by: gauraV <gauravsaini964@gmail.com>

--- src/components/Screens/Mapping/ServiceModuleMapping.jsx
+++ src/components/Screens/Mapping/ServiceModuleMapping.jsx
@@ -1,13 +1,16 @@
-import React, { useState } from "react";
-import { useSelector } from "react-redux";
-import { Card, Form, message } from "antd";
-import MButton from "../../Elements/MButton";
-import { mapServiceModule,getAlreadyMappedModuleServices } from "../../../actions";
-import Services from "../../Elements/Services";
-import Modules from "../../Elements/Modules";
+import React, { useState } from 'react';
+import { useSelector } from 'react-redux';
+import { Card, Form, message } from 'antd';
+import MButton from '../../Elements/MButton';
+import {
+  mapServiceModule,
+  getAlreadyMappedModuleServices
+} from '../../../actions';
+import Services from '../../Elements/Services';
+import Modules from '../../Elements/Modules';
 
 const ServiceModuleMapping = props => {
-  const [moduleServices,setModuleServices] = useState([]);
+  const [moduleServices, setModuleServices] = useState([]);
   const user = useSelector(state => state.userAuth);
   const onSubmit = e => {
     e.preventDefault();
@@ -19,26 +22,26 @@ const ServiceModuleMapping = props => {
         };
         const response = await mapServiceModule(user.Authorization, values);
         if (response.status === 200) {
-          message.success("Mapped successfully");
+          message.success('Mapped successfully');
         }
       }
     });
   };
-  
-  
- const onServiceChange = async service_id => {
-  const response = await getAlreadyMappedModuleServices(user.Authorization, service_id)
-  //console.log(response.data.result);
-  let tempList = [];
-  if( response.data.result.length>0){
-    for(let i = 0; i <response.data.result.length ; i++){
-      tempList.push(response.data.result[i].module__name)
-    }    
-    setModuleServices(tempList)
-  }
- 
-}
- 
+
+  const onServiceChange = async service_id => {
+    const response = await getAlreadyMappedModuleServices(
+      user.Authorization,
+      service_id
+    );
+    //console.log(response.data.result);
+    let tempList = [];
+    if (response.data.result.length > 0) {
+      for (let i = 0; i < response.data.result.length; i++) {
+        tempList.push(response.data.result[i].module_id);
+      }
+      setModuleServices(tempList);
+    }
+  };
 
   const onServiceModuleChange = value => {
     setModuleServices(value);
@@ -49,17 +52,15 @@ const ServiceModuleMapping = props => {
     <Card title="Service Module Mapping">
       <Form onSubmit={onSubmit}>
         <Form.Item label="Services">
-          {getFieldDecorator("service_id", {
+          {getFieldDecorator('service_id', {
             rules: [{ required: true }]
           })(<Services onChange={onServiceChange} />)}
         </Form.Item>
         <Form.Item label="Modules">
-          {getFieldDecorator("module_id_list", {
-            rules: [{ required: true }], initialValue: moduleServices
-          })(<Modules
-             mode="multiple"
-             onChange={onServiceModuleChange} 
-          />)}
+          {getFieldDecorator('module_id_list', {
+            rules: [{ required: true }],
+            initialValue: moduleServices
+          })(<Modules mode="multiple" onChange={onServiceModuleChange} />)}
         </Form.Item>
         <MButton>Submit</MButton>
       </Form>
