3743e0b5f9e758f51534183d26130a50bca352b0
videsh <videsh.ghodarop@iaugmentor.com>
2019-12-02 09:39:00 +0530

    commit for pull from branch prateek

--- src/components/Screens/TechnicalAssesment/TechnicalAssesmentDetails.jsx
+++ src/components/Screens/TechnicalAssesment/TechnicalAssesmentDetails.jsx
@@ -49,7 +49,7 @@ const TechnicalAssesmentDetails = props => {
           AuthToken,
           search
         );
-        // console.log(response.data.result);
+
         setData(response.data.result);
         setLoading(false);
       } catch (error) {
