aabf5c37fd37fcb0dd56611ab3cc0552900589f9
Shagun Sharma <shagun.sharma@iaugmentor.com>
2020-01-03 15:15:46 +0530

    category layout changed

--- src/components/Screens/WouldYouRather/Tree/MapParameters.jsx
+++ src/components/Screens/WouldYouRather/Tree/MapParameters.jsx
@@ -1,10 +1,21 @@
 import React, { useState, useEffect } from "react";
-import { useSelector } from "react-redux";
-import { Modal, Card, Form, message, Icon } from "antd";
+import { useSelector, useDispatch } from "react-redux";
+import {
+  Modal,
+  Card,
+  Form,
+  message,
+  Icon,
+  Select,
+  Button,
+  Tag,
+  Table
+} from "antd";
 import MButton from "../../../Elements/MButton";
 import Parameters from "../../../Elements/Parameters";
 import Categories from "../../../Elements/Categories";
 import {
+  fetchCategories,
   wyrTreeMapParameters,
   wyrTreeList,
   deleteMappedParameter
@@ -12,19 +23,54 @@ import {
 import "./index";
 
 const EpisodeParameterMap = props => {
+  const dispatch = useDispatch();
   const user = useSelector(state => state.userAuth);
+  const categories = useSelector(state => state.category.categories);
 
   const actionId = props.actionId;
   const technical_service_id = props.selectedTechnicalId;
   const [cardLoading, setCardLoading] = useState(false);
 
-  const [category, setCategory] = useState(null);
   const [selectedParameters, setSelectedParameters] = useState([]);
   const [alreadyMappedParameters, setAlreadyMappedParameters] = useState([]);
+
+  const [selectedCategory, setSelectedCategory] = useState(null);
+
+  const [finalSelectedList, setFinalSelectedList] = useState([]);
+  const [isDisabled, setIsDisabled] = useState(false);
+
+  const columns = [
+    {
+      title: "Category Id",
+      dataIndex: "category_id",
+      key: "category_id"
+    },
+    {
+      title: "Category Name",
+      dataIndex: "category_name",
+      key: "category_name"
+    },
+    {
+      title: "Parameters",
+      key: "parameters",
+      dataIndex: "parameters",
+      render: parameters => (
+        <span>
+          {parameters.map(param => {
+            return <Tag key={param.id}>{param.name}</Tag>;
+          })}
+        </span>
+      )
+    }
+  ];
 
   useEffect(() => {
     const fetchAlreadyMappedList = async () => {
       setCardLoading(true);
+
+      try {
+        dispatch(fetchCategories(user.Authorization));
+      } catch (error) {}
 
       try {
         const response = await wyrTreeList(
@@ -85,7 +131,7 @@ const EpisodeParameterMap = props => {
       }
     };
     fetchAlreadyMappedList();
-  }, [user.Authorization, actionId, technical_service_id]);
+  }, [user.Authorization, actionId, technical_service_id, dispatch]);
 
   const onSubmit = e => {
     e.preventDefault();
@@ -117,10 +163,12 @@ const EpisodeParameterMap = props => {
   };
 
   const onCategoryChange = value => {
-    setCategory(value);
+    console.log(value);
+    setSelectedCategory(value);
   };
 
   const onParameterChange = value => {
+    console.log(value);
     // setParameters(value);
   };
 
@@ -133,6 +181,32 @@ const EpisodeParameterMap = props => {
     try {
       await deleteMappedParameter(user.Authorization, gameName.id);
     } catch (error) {}
+  };
+
+  const addComptency = () => {
+    let tempList = {
+      category_id: selectedCategory,
+      category_name: "xyz",
+      parameters: [
+        {
+          id: 1,
+          name: "param 1"
+        },
+        {
+          id: 2,
+          name: "param 2"
+        }
+      ]
+    };
+
+    setFinalSelectedList([...finalSelectedList, tempList]);
+    // set parameter list to null
+    setIsDisabled(true);
+    setSelectedCategory(null);
+  };
+
+  const addNewCategory = () => {
+    setIsDisabled(false);
   };
 
   const { getFieldDecorator } = props.form;
@@ -149,7 +223,85 @@ const EpisodeParameterMap = props => {
           bodyStyle={{ padding: "0px" }}
           bordered={false}
           loading={cardLoading}
-        ></Card>
+        >
+          <div style={{ textAlign: "right", marginBottom: "20px" }}>
+            <Icon
+              onClick={addNewCategory}
+              type="plus"
+              style={{ color: "blue", fontSize: "16px", cursor: "pointer" }}
+            ></Icon>
+          </div>
+
+          <Form>
+            <div
+              style={{
+                border: "1px solid #999999",
+                borderRadius: "5px",
+                padding: "20px",
+                marginBottom: "40px"
+              }}
+            >
+              <Form.Item>
+                {getFieldDecorator("competency", {
+                  rules: [{ required: true }]
+                })(
+                  <Select
+                    mode={"default"}
+                    placeholder="Select a category"
+                    onChange={onCategoryChange}
+                    showSearch
+                    allowClear
+                    defaultValue={selectedCategory}
+                    value={selectedCategory}
+                    disabled={isDisabled === true ? true : false}
+                  >
+                    {categories.map(category => {
+                      return (
+                        <Select.Option key={category.id} value={category.id}>
+                          {category.name}
+                        </Select.Option>
+                      );
+                    })}
+                  </Select>
+                )}
+              </Form.Item>
+
+              {selectedCategory === null ? (
+                <div>Select category to view parameters list</div>
+              ) : (
+                <Form.Item label="Parameters">
+                  {getFieldDecorator("parameter", {
+                    rules: [{ required: true }]
+                    // initialValue: selectedParameters
+                  })(
+                    <Parameters
+                      mode="multiple"
+                      onChange={onParameterChange}
+                      onDeselect={e => onDeselectingParameter(e)}
+                      categories={[selectedCategory]}
+                    />
+                  )}
+                </Form.Item>
+              )}
+
+              <div style={{ textAlign: "center", marginTop: "30px" }}>
+                <Button
+                  type="primary"
+                  onClick={addComptency}
+                  disabled={isDisabled === true ? true : false}
+                >
+                  Add Competencies
+                </Button>
+              </div>
+            </div>
+          </Form>
+
+          {/* ///////////////////////////////////////////////////////////// */}
+
+          {finalSelectedList.length === 0 ? null : (
+            <Table columns={columns} dataSource={finalSelectedList} />
+          )}
+        </Card>
       </Modal>
     </div>
   );
