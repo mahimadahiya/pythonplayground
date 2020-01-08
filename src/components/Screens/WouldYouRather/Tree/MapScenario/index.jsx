912b62b500b3e0033ca15f2c944186341c94cb48
Shagun Sharma <shagun.sharma@iaugmentor.com>
2020-01-08 17:29:13 +0530

    scenario list added

--- src/components/Screens/WouldYouRather/Tree/MapScenario/index.jsx
+++ src/components/Screens/WouldYouRather/Tree/MapScenario/index.jsx
@@ -11,7 +11,12 @@ const MapScenarioModal = props => {
 
   const [loading, setLoading] = useState(false);
 
-  const [scenarioList, setScenarioList] = useState([]);
+  const [scenarioList, setScenarioList] = useState({
+    "1": {
+      wyr_action_list: [],
+      wyr_scenario_id: null
+    }
+  });
 
   const [selectDropdownScenarioList, setSelectDropdownScenarioList] = useState(
     []
@@ -25,15 +30,39 @@ const MapScenarioModal = props => {
         const response = await wyrScenarioList(user.Authorization, techId);
 
         if (response.result.wyr_scenario_list.length > 0) {
-          setSelectDropdownScenarioList(response.result.wyr_scenario_list);
+          let ResponseList = response.result.wyr_scenario_list;
+
+          for (let i = 0; i < ResponseList.length; i++) {
+            ResponseList[i]["is_selected"] = false;
+          }
 
           if (
-            props.sceneDetails.scenario_details !== null ||
-            props.sceneDetails.scenario_details !== undefined
+            props.sceneDetails.scenario_details === null ||
+            props.sceneDetails.scenario_details === undefined
           ) {
-            if (Object.keys(props.sceneDetails.scenario_details).length !== 0) {
-              console.log(props.sceneDetails.scenario_details);
+            console.log("object");
+            setSelectDropdownScenarioList(ResponseList);
+          } else {
+            if (Object.keys(props.sceneDetails.scenario_details).length === 0) {
+              setSelectDropdownScenarioList(ResponseList);
+            } else {
+              let PropsList = props.sceneDetails.scenario_details;
+
+              for (let i = 0; i < ResponseList.length; i++) {
+                for (
+                  let j = 1;
+                  j <= Object.keys(props.sceneDetails.scenario_details).length;
+                  j++
+                ) {
+                  if (ResponseList[i].id === PropsList[j].wyr_scenario_id) {
+                    ResponseList[i]["is_selected"] = true;
+                  }
+                }
+              }
+              setScenarioList(PropsList);
             }
+            console.log(">>>>>>");
+            setSelectDropdownScenarioList(ResponseList);
           }
         } else {
           setSelectDropdownScenarioList([]);
@@ -53,40 +82,48 @@ const MapScenarioModal = props => {
       loading={loading}
       bordered={false}
     >
-      <div
-        style={{
-          border: "1px solid #e6e6e6",
-          borderRadius: 5,
-          margin: "20px 0",
-          padding: "20px"
-        }}
-      >
+      {console.log(Object.keys(scenarioList))}
+      {Object.keys(scenarioList).map((key, index) => (
         <div
-          style={{ marginBottom: "20px", fontWeight: 600, color: "#000000" }}
+          key={index}
+          style={{
+            border: "1px solid #e6e6e6",
+            borderRadius: 5,
+            margin: "20px 0",
+            padding: "20px"
+          }}
         >
-          Scenario 1
-        </div>
-
-        {/* scenario list */}
-        <div style={{ display: "flex" }}>
-          <div style={{ width: "200px", margin: "auto 0" }}>
-            Select Scenario
+          <div
+            style={{ marginBottom: "20px", fontWeight: 600, color: "#000000" }}
+          >
+            Scenario 1
           </div>
-          <div style={{ width: "100%" }}>
-            <Select style={{ width: "100%" }}>
-              {selectDropdownScenarioList.map(item => (
-                <Select.Option key={item.id} value={item.id}>
-                  {item.objective}
-                </Select.Option>
-              ))}
-            </Select>
-          </div>
-        </div>
-        {/* scenario end */}
 
-        {/* actions starts */}
-        {/* actions ends */}
-      </div>
+          {/* scenario list */}
+          <div style={{ display: "flex" }}>
+            <div style={{ width: "200px", margin: "auto 0" }}>
+              Select Scenario
+            </div>
+            <div style={{ width: "100%" }}>
+              <Select style={{ width: "100%" }} placeholder="Select Scenario">
+                {selectDropdownScenarioList.map(item => (
+                  <Select.Option
+                    key={item.id}
+                    value={item.id}
+                    disabled={item.is_selected}
+                  >
+                    {item.objective}
+                  </Select.Option>
+                ))}
+              </Select>
+            </div>
+          </div>
+          {/* scenario end */}
+
+          {/* actions starts */}
+          {/* actions ends */}
+        </div>
+      ))}
 
       <Button type="dashed">
         <Icon type="plus-circle" /> Add More Scenario
