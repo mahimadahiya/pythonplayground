d2ce4cf47789f882c4fafb36c9c959b6c6dfd8ca
Shagun Sharma <shagun.sharma@iaugmentor.com>
2020-01-08 13:43:35 +0530

    scenario list dropdown added

--- src/components/Screens/WouldYouRather/Tree/MapScenario/index.jsx
+++ src/components/Screens/WouldYouRather/Tree/MapScenario/index.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { Card, Select, Input, Icon, Button, message } from "antd";
-import { wyrEpisodeSceneUpdate } from "../../../../../actions";
+import { wyrEpisodeSceneUpdate, wyrScenarioList } from "../../../../../actions";
 import { useSelector } from "react-redux";
 
 const MapScenarioModal = props => {
@@ -11,12 +11,87 @@ const MapScenarioModal = props => {
 
   const [loading, setLoading] = useState(false);
 
+  const [scenarioList, setScenarioList] = useState([]);
+
+  const [selectDropdownScenarioList, setSelectDropdownScenarioList] = useState(
+    []
+  );
+
+  useEffect(() => {
+    const callDataApi = async () => {
+      setLoading(true);
+      try {
+        let techId = props.sceneDetails.technical_service_id;
+        const response = await wyrScenarioList(user.Authorization, techId);
+
+        if (response.result.wyr_scenario_list.length > 0) {
+          setSelectDropdownScenarioList(response.result.wyr_scenario_list);
+
+          if (
+            props.sceneDetails.scenario_details !== null ||
+            props.sceneDetails.scenario_details !== undefined
+          ) {
+            if (Object.keys(props.sceneDetails.scenario_details).length !== 0) {
+              console.log(props.sceneDetails.scenario_details);
+            }
+          }
+        } else {
+          setSelectDropdownScenarioList([]);
+        }
+        setLoading(false);
+      } catch (error) {
+        setLoading(false);
+      }
+    };
+
+    callDataApi();
+  }, [user.Authorization]);
+
   return (
     <Card
       bodyStyle={{ padding: 0, fontSize: "15px" }}
       loading={loading}
       bordered={false}
-    ></Card>
+    >
+      <div
+        style={{
+          border: "1px solid #e6e6e6",
+          borderRadius: 5,
+          margin: "20px 0",
+          padding: "20px"
+        }}
+      >
+        <div
+          style={{ marginBottom: "20px", fontWeight: 600, color: "#000000" }}
+        >
+          Scenario 1
+        </div>
+
+        {/* scenario list */}
+        <div style={{ display: "flex" }}>
+          <div style={{ width: "200px", margin: "auto 0" }}>
+            Select Scenario
+          </div>
+          <div style={{ width: "100%" }}>
+            <Select style={{ width: "100%" }}>
+              {selectDropdownScenarioList.map(item => (
+                <Select.Option key={item.id} value={item.id}>
+                  {item.objective}
+                </Select.Option>
+              ))}
+            </Select>
+          </div>
+        </div>
+        {/* scenario end */}
+
+        {/* actions starts */}
+        {/* actions ends */}
+      </div>
+
+      <Button type="dashed">
+        <Icon type="plus-circle" /> Add More Scenario
+      </Button>
+    </Card>
   );
 };
 
